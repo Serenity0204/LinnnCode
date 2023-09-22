49c9e89d279b1eaaac8911215fe5a9434975fbf0
serenity0204 <yuhenglin0204@gmail.com>
2023-09-22 09:55:06 -0700

    added utils and basic model

--- linnncode/models.py
+++ linnncode/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Problem(models.Model):
+    pass
+
+
+## has 3 fields, c++, python, and javascript, and will be associate to problem
+class Solution(models.Model):
+    pass
+
+
+# will have list of test cases into text field
+class TestSuite(models.Model):
+    pass
